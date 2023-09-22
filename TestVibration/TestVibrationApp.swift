5238cfcde82b53d28171a7feeab1d630adc8d9e2
gwair1989 <gwair1989@gmail.com>
2023-09-22 16:03:47 +0300

    Initial Commit

---
+++ TestVibration/TestVibrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVibrationApp.swift
+//  TestVibration
+//
+//  Created by mac book on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestVibrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
